8cf92b38794b8a35e9c10fd96c95d59953821160
Priya28297 <priyaprasath282@gmail.com>
2023-07-26 15:55:58 +0530

    Initial commit

---
+++ src/firstpackage/Dayone.java
@@ -0,0 +1,12 @@
+package firstpackage;
+
+public class Dayone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Name   : Priya");
+System.out.println("Mobile : 9445653102");
+System.out.println("Age    : 26");
+	}
+
+}
